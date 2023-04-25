8fc63345b9fa567ccb2fb9c3766d6fd613b11577
Merugul-Rahul <122848686+Merugul-Rahul@users.noreply.github.com>
2023-04-25 18:21:01 -0400

    Update mpi_ping_pong.cpp

--- pingpong/mpi_ping_pong.cpp
+++ pingpong/mpi_ping_pong.cpp
@@ -1,15 +1,33 @@
 #include <mpi.h>
 #include <iostream>
 
-int main (int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
+    int rank, size, value;
 
-  if (argc < 2) {
-    std::cerr<<"usage: mpirun "<<argv[0]<<" <value>"<<std::endl;
-    return -1;
-  }
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  
+    if (argc < 2) {
+        if (rank == 0) {
+            std::cerr << "usage: mpirun " << argv[0] << " <value>" << std::endl;
+        }
+        MPI_Finalize();
+        return -1;
+    }
 
+    value = std::stoi(argv[1]);
 
-  return 0;
+    if (rank == 0) {
+        MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::cout << "Final value: " << value << std::endl;
+    } else if (rank == 1) {
+        MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        value += 2;
+        MPI_Send(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
